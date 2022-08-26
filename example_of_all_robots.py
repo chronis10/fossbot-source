98b2248a05f82c4c8fc6f2b9ee801bcfdefdb0fb
manouslinard <it22064@hua.gr>
2022-08-26 04:02:54 +0300

    implemented fossbot_lite+ultimate_test

--- example_of_all_robots.py
+++ example_of_all_robots.py
@@ -13,6 +13,26 @@ def main(robot: robot_interface.FossBotInterface) -> None:
     time.sleep(10)
     robot.stop()
 
+def ultimate_test(robot: robot_interface.FossBotInterface) -> None:
+    '''Test important function of robot'''
+    robot.move_distance(21)
+    robot.wait(1)
+    robot.rotate_clockwise_90()
+    robot.rotate_counterclockwise_90()
+    robot.wait(1)
+    print(f'Light Sensor: {robot.get_light_sensor()}')
+    print(f'Dark: {robot.check_for_dark()}')
+    print(f'Ultrasonic distance: {robot.get_distance()}')
+    print(f'Obstacle => {robot.check_for_obstacle()}')
+    print(f'Gyroscope: {robot.get_gyroscope()}')
+    print(f'Acceleration: {robot.get_acceleration()}')
+    print(f'MiddleSensor: {robot.get_floor_sensor(1)}, On Line? => {robot.check_on_line(1)}')
+    print(f'RightSensor: {robot.get_floor_sensor(2)}, On Line? => {robot.check_on_line(2)}')
+    print(f'LeftSensor: {robot.get_floor_sensor(3)}, On Line? => {robot.check_on_line(3)}')
+    print('Changing led colors...')
+    change_color(robot)
+
+
 def change_color(robot: robot_interface.FossBotInterface) -> None:
     ''' Changes the color of a led for some times '''
     for i in range(3):
@@ -57,4 +77,6 @@ if __name__ == "__main__":
     # Call a the same routine for the real and the simulated robot
     main(my_real_robot)
 
-    change_color(my_simu_robot)
+    main(my_simu_robot)
+    #ultimate_test(my_simu_robot)
+    #change_color(my_simu_robot)
